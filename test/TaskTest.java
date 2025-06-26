235539f65145d618f2de0c51892b98abb4c2f83f
Yousra0225 <chbibyousra02@gmail.com>
2025-06-26 15:54:53 +0200

    feat(tasktest): add tasktest class

---
+++ test/TaskTest.java
@@ -0,0 +1,10 @@
+public class TaskTest {
+    public static void main(String[] args) {
+        Task task = new Task("Test unitaire");
+        assert !task.isDone();
+        task.markDone();
+        assert task.isDone();
+        System.out.println("Tests passed.");
+    }
+}
+
