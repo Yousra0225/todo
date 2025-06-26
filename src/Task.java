ce537f242f830b493a19e2b0aed0ce5ffb21597e
Yousra0225 <chbibyousra02@gmail.com>
2025-06-26 15:54:17 +0200

    feat(task): add task class

---
+++ src/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String title;
+    private boolean done;
+
+    public Task(String title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
