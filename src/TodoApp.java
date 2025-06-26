2d4c12c07fccdd8cad0206299e8fd147d47a411e
Yousra0225 <chbibyousra02@gmail.com>
2025-06-26 15:54:30 +0200

    feat(task): add todo app class

---
+++ src/TodoApp.java
@@ -0,0 +1,8 @@
+public class TodoApp {
+    public static void main(String[] args) {
+        Task task = new Task("Finir le projet");
+        System.out.println("Task: " + task.getTitle());
+        task.markDone();
+        System.out.println("Done: " + task.isDone());
+    }
+}
