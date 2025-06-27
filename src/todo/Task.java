5321615ab0b58b676f324d3f9e695007765395f4
Yousra0225 <chbibyousra02@gmail.com>
2025-06-27 10:02:58 +0200

    move java files to todo package

---
+++ src/todo/Task.java
@@ -0,0 +1,22 @@
+package todo;
+public class Task {
+    private String title;
+    private boolean done;
+
+    public Task(String title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
